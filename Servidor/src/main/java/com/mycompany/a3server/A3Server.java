6d4dd0a5995c9c5fe97c3f4d64fed462e8c5de7c
LeitGCastro <guiadse@gmail.com>
2022-11-10 22:23:19 -0300

    Arquivo do servidor

---
+++ Servidor/src/main/java/com/mycompany/a3server/A3Server.java
@@ -0,0 +1,56 @@
+package com.mycompany.a3server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class A3Server {
+    Socket socketClient;
+    ServerSocket serversocket;
+
+    public boolean connect() {
+        try {
+            socketClient = serversocket.accept();  // fase de conexao
+            return true;
+        } catch (IOException e) {
+            System.err.println("Nao fez conexao" + e.getMessage());
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        try {
+            A3Server servidor = new A3Server();
+            servidor.rodarServidor();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    } 
+
+    public void rodarServidor() throws Exception {
+        String textoRecebido = "";
+        String textoEnviado = "Olá, Cliente";
+        Scanner input = new Scanner(System.in);
+
+        serversocket = new ServerSocket(9500);
+        System.out.println("Servidor iniciado!");
+
+        while(true) {
+            if (connect()) {
+                textoRecebido = Conexao.receber(socketClient);
+
+                System.out.println("Cliente enviou: " + textoRecebido);
+                System.out.print("\nDigite a sua mensagem: ");// fase de dados
+                textoEnviado = input.nextLine();
+                Conexao.enviar(socketClient, textoEnviado);
+                socketClient.close();
+            }
+        }
+    }
+
+}
